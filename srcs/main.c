154efcb135f9c94febdaf9e4d6a5feed327ac196
Wei Jian Hoe <wjhoe@c2r5s4.42singapore.sg>
2025-06-18 16:55:02 +0800

    changes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: wjhoe <wjhoe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 10:05:50 by wjhoe             #+#    #+#             */
-/*   Updated: 2025/06/18 15:51:19 by wjhoe            ###   ########.fr       */
+/*   Updated: 2025/06/18 16:51:13 by wjhoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ int main (int ac, char **av, char **envp)
 	t_data	*data;
 	
 	data = init_data(ac, av, envp);
+	printf(">> hi << \n");
 	ft_pipe(data);
 }
 
-// not invalidate a bad command 
-// if i cannot access the file, i can open devnull
